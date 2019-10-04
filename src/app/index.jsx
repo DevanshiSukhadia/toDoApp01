4c6ddbea54e3a183ffe27d8b273fdc71793cc84d
Devanshi Sukhadia <devanshisukhadia@devanshis-mbp.attlocal.net>
2019-10-04 10:10:50 -0700

    Work in Progress

---
+++ src/app/index.jsx
@@ -0,0 +1,11 @@
+import { store } from './store'
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Main } from './component/Main'
+//import { Dashboard } from './component/Dashboard'
+
+ReactDOM.render(
+    < Main/>,
+    document.getElementById("app")
+); 
